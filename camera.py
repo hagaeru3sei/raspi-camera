ca4f2050ee51b3919945a946fe9a7c6dadd12b6a
Nobuaki Mochizuki <hagaeru3sei@gmail.com>
2018-09-17 10:55:01 +0900

    Add capturing jpg image method

--- camera.py
+++ camera.py
@@ -8,6 +8,7 @@ import picamera
 import threading
 import wiringpi as pi
 
+
 pid = os.getpid()
 
 
@@ -31,6 +32,27 @@ class InfraredObserver(object):
         threading.Thread(target=self._execute, args=(callback,)).start()
 
 
+class ButtonObserver(object):
+    """ タクトスイッチ監視
+    """
+
+    pir_pin = 27
+
+    def __init__(self):
+        pi.wiringPiSetupGpio()
+        pi.pinMode(self.pir_pin, pi.INPUT)
+
+    def _execute(self, callback):
+        while True:
+            if pi.digitalRead(self.pir_pin) == pi.HIGH:
+                callback()
+            # チャタリング防止
+            time.sleep(0.2)
+
+    def observe(self, callback):
+        threading.Thread(target=self._execute, args=(callback,)).start()
+
+
 class Camera(object):
 
     is_recording = False    
@@ -42,6 +64,15 @@ class Camera(object):
 
     def __init__(self):
         pass
+
+    def capture(self) -> None:
+        current_time = datetime.datetime.now().strftime('%Y%m%d%H%M%S')
+        timestamp = math.ceil(datetime.datetime.now().timestamp())
+        with picamera.PiCamera() as camera:
+            camera.resolution = (self.resolution[0], self.resolution[1],)
+            camera.start_preview()
+            camera.capture('%s/capure-%s-%s.%s' % (self.data_dir, timestamp, current_time, self.ext_image,))
+            camera.stop_preview()
 
     def caputure_movie(self) -> None:
         if self.is_recording:
@@ -70,15 +101,17 @@ def signal_handler(signalnum, frame):
 
 
 def main():
-    camera = Camera()
-    infrared = InfraredObserver()
-    infrared.observe(camera.caputure_movie)
-
     # signal handler
     signal.signal(signal.SIGINT, signal_handler)  # Ctrl-C
     signal.signal(signal.SIGTERM, signal_handler)  # kill -term [PID]
     signal.signal(signal.SIGHUP, signal_handler)  # kill -HUP [PID]
     signal.signal(signal.SIGQUIT, signal_handler)  # Quit
+
+    camera = Camera()
+    infrared = InfraredObserver()
+    infrared.observe(camera.caputure_movie)
+    button = ButtonObserver()
+    button.observe(camera.capture)
 
     while True:
         time.sleep(1)
